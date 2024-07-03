ba864b5852a54a5888f110ea04b3a79262409036
koudaihirata <2230051@ecc.ac.jp>
2024-07-03 22:03:46 +0900

    RankingBtnにpropsを実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,25 @@ import { useEffect, useState } from "react";
 import { css } from "../../styled-system/css";
 import RankingBtn from "@/components/RankingBtn";
 
+const RankingProps = [
+  {
+    User:"ストッくん",
+    query:"の余り物",
+    One:"キャベツ",
+  },
+  {
+    User:"ストッくん",
+    query:"におすすめな物",
+    One:"牛肉",
+  },
+]
+
 export default function Home() {
   return (
     <main className={css({width:'100%',height:'50vh',background:'linear-gradient(180deg, #FFCE7B 58.71%, #FFF 100%)',position:'relative',marginTop:'52px'})}>
-      <RankingBtn/>
-      <RankingBtn/>
+      {RankingProps.map((RankingProp,index) => {
+        return<RankingBtn key={index} {...RankingProp}/>
+      })}
     </main>
   );
 }

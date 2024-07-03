2e579ab9c5370873870a0cd31adb568b995b3d25
koudaihirata <2230051@ecc.ac.jp>
2024-07-03 22:59:06 +0900

    BlueCircle.svgを追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { useEffect, useState } from "react";
 import { css } from "../../styled-system/css";
 import RankingBtn from "@/components/RankingBtn";
+import Link from "next/link";
 
 const RankingProps = [
   {
@@ -23,11 +24,24 @@ export default function Home() {
       {RankingProps.map((RankingProp,index) => {
         return<RankingBtn key={index} {...RankingProp}/>
       })}
-      <section>
+      <section className={css({width:'330px',margin:'75px auto 0'})}>
         <h3>キャンペーン・お得情報</h3>
-        <div className={css({width:'330px',height:'116px',background:'url()'})}>
+        <Link href="#">
+          <div className={css({
+            width:'330px',
+            height:'116px',
+            bgImage:'url(/coupon.svg)',
+            bgPosition: 'center',
+            position:'relative',
+            _before: {
+              content:'',
+              position:'absolute',
+              bgImage:'url(/BlueCircle.svg)',
+            }
+          })}>
 
-        </div>
+          </div>
+        </Link>
       </section>
     </main>
   );

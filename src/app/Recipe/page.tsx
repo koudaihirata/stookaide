271f1434c1c985ef3fe5f9907e8321e0b842d6c7
koudaihirata <2230051@ecc.ac.jp>
2024-07-04 22:35:52 +0900

    楽天レシピAPIの検索機能追加

--- src/app/Recipe/page.tsx
+++ src/app/Recipe/page.tsx
@@ -1,5 +1,4 @@
 "use client"
-import Image from 'next/image';
 import { useEffect, useState } from 'react';
 import { css } from '../../../styled-system/css';
 import Link from 'next/link';
@@ -11,9 +10,19 @@ type Recipe = {
     Indication:string;
     recipeUrl:string;
 };
+type Category = {
+    category1: string;
+    category2: string;
+    category3: string;
+    categoryId: string;
+    categoryName: string;
+};
 
 export default function Recipe() {
     const [recipes, setRecipes] = useState<Recipe[]>([]);
+    const [keyword, setKeyword] = useState("");
+    const [categories, setCategories] = useState<Category[]>([]);
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
         const url = `https://app.rakuten.co.jp/services/api/Recipe/CategoryRanking/20170426?applicationId=${process.env.NEXT_PUBLIC_RAKUTEN_API_KEY}`;
@@ -34,12 +43,102 @@ export default function Recipe() {
             .catch(error => console.error('Error:', error));
     }, []);
 
+    const handleSearch = async (event: React.FormEvent) => {
+        event.preventDefault();
+        setLoading(true);
+        const url = `https://app.rakuten.co.jp/services/api/Recipe/CategoryList/20170426?applicationId=${process.env.NEXT_PUBLIC_RAKUTEN_API_KEY}`;
+    
+        const res = await fetch(url);
+        const data = await res.json();
+    
+        let parentDict: { [key: string]: string } = {};
+        let df: Category[] = [];
+    
+        for (let category of data.result.large) {
+            df.push({
+                category1: category.categoryId,
+                category2: "",
+                category3: "",
+                categoryId: category.categoryId,
+                categoryName: category.categoryName,
+            });
+        }
+    
+        for (let category of data.result.medium) {
+            df.push({
+                category1: category.parentCategoryId,
+                category2: category.categoryId,
+                category3: "",
+                categoryId: category.parentCategoryId + "-" + category.categoryId,
+                categoryName: category.categoryName,
+            });
+            parentDict[category.categoryId] = category.parentCategoryId;
+        }
+    
+        for (let category of data.result.small) {
+            df.push({
+                category1: parentDict[category.parentCategoryId],
+                category2: category.parentCategoryId,
+                category3: category.categoryId,
+                categoryId:
+                parentDict[category.parentCategoryId] +
+                "-" +
+                category.parentCategoryId +
+                "-" +
+                category.categoryId,
+                categoryName: category.categoryName,
+            });
+        }
+    
+        const dfKeyword = df.filter((row) => row.categoryName.includes(keyword));
+        let dfRecipe: Recipe[] = [];
+        let displayedRecipeIds = new Set();
+    
+        for (let row of dfKeyword) {
+            await new Promise((resolve) => setTimeout(resolve, 1000));
+        
+            let recipeUrl = `https://app.rakuten.co.jp/services/api/Recipe/CategoryRanking/20170426?applicationId=${process.env.NEXT_PUBLIC_RAKUTEN_API_KEY}&categoryId=${row.categoryId}`;
+            let res = await fetch(recipeUrl);
+            let jsonData = await res.json();
+            let recipes = jsonData.result;
+        
+            recipes.forEach((recipe:any) => {
+                if (!displayedRecipeIds.has(recipe.recipeId)) {
+                dfRecipe.push({
+                    image: recipe.foodImageUrl,
+                    title: recipe.recipeTitle,
+                    description: recipe.recipeDescription,
+                    Indication: recipe.recipeIndication,
+                    recipeUrl: recipe.recipeUrl,
+                });
+                displayedRecipeIds.add(recipe.recipeId);
+                }
+            });
+        }
+    
+        setRecipes(dfRecipe);
+        setLoading(false);
+    };
+    
+
     return (
         <main>
             <div className={css({width:'85%',margin:'0 auto'})}>
                 <section className={css({width:'100%',height:'30px',borderBottom:'2px solid #333',display:'flex',alignItems:'center'})}>
                     <h2>レシピ一覧</h2>
                 </section>
+                <form onSubmit={handleSearch}>
+                    <input
+                        type="text"
+                        value={keyword}
+                        onChange={(e) => setKeyword(e.target.value)}
+                        placeholder="キーワードを入力"
+                    />
+                    <button type="submit">検索</button>
+                </form>
+                {loading ? (
+                <p>Loading...</p>
+                ) : (
                 <ul id="recipe_list">
                     {recipes.map((recipe, index) => (
                         <Link key={index} href={recipe.recipeUrl} legacyBehavior passHref>
@@ -54,6 +153,7 @@ export default function Recipe() {
                         </Link>
                     ))}
                 </ul>
+                )}
             </div>
         </main>
     );

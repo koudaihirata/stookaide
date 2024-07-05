dcc9af2a622fbbc81357bff91e39fa2430c61aa5
koudaihirata <2230051@ecc.ac.jp>
2024-07-05 09:32:22 +0900

    ヘルプの文字の色と太さの変更

--- src/components/Help.tsx
+++ src/components/Help.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { css } from "../../styled-system/css";
+import { white } from "@/style/color";
 
 type HelpProps = {
     item: string,
@@ -10,7 +11,7 @@ export default function Help(Props:HelpProps) {
     return(
         <Link href={Props.link}>
             <div className={css({width:'330px',height:'40px',bg:'linear-gradient(90deg, #FFCE7B 50.15%, #FFF 100%)',borderRadius:'8px',display:'flex',alignItems:'center',marginTop:'10px'})}>
-                <p className={css({marginLeft:'24px'})}>{Props.item}</p>
+                <p className={css({marginLeft:'24px',fontWeight:'bold'})} style={{color:white}}>{Props.item}</p>
             </div>
         </Link>
     )

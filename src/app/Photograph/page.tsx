2d31b7fb5794ca09d72fda1c6816486c3d523369
koudaihirata <2230051@ecc.ac.jp>
2024-07-06 19:26:17 +0900

    レイアウト確認

--- src/app/Photograph/page.tsx
+++ src/app/Photograph/page.tsx
@@ -1,6 +1,8 @@
 "use client"; 
 import { useState, useEffect } from 'react';
 import { css } from '../../../styled-system/css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
 
 export default function Photograph() {
     const [detectedObjects, setDetectedObjects] = useState<Set<string>>(new Set<string>());
@@ -22,11 +24,13 @@ export default function Photograph() {
                 const detectedObjects: string[] = await response.json();
                 
                 // 翻訳が存在するオブジェクトのみをセットに追加
-                const newDetectedObjects = new Set<string>(
-                    detectedObjects
-                        .filter((obj: string) => translations.hasOwnProperty(obj))
-                        .map((obj: string) => translations[obj])
-                );
+                // const newDetectedObjects = new Set<string>(
+                //     detectedObjects
+                //         .filter((obj: string) => translations.hasOwnProperty(obj))
+                //         .map((obj: string) => translations[obj])
+                // );
+                // 検出したもの全てを追加
+                const newDetectedObjects = new Set(detectedObjects.map((obj: string) => translations[obj] || obj));
                 setDetectedObjects(prevObjects => new Set<string>([...prevObjects, ...newDetectedObjects]));
             } catch (error) {
                 console.error('Error fetching detections:', error);
@@ -40,11 +44,19 @@ export default function Photograph() {
 
     return (
         <main>
-            <h2 className={css({fontSize:'20px',fontWeight:'bold',textAlign:'center'})}>食材をカメラに移してください</h2>
-            <img id="videoFeed" src="http://localhost:5001/detect" alt="ビデオフィード" style={{ width: '100%', height: '480px', backgroundColor: 'black' }} />
-            <h2>検出結果:</h2>
-            <ul>
-                {Array.from(detectedObjects).map((obj, index) => <li key={index}>{obj}</li>)}
+            <h2 className={css({fontSize:'20px',fontWeight:'bold',textAlign:'center',pt:'10px'})}>食材を撮影してください</h2>
+            <img id="videoFeed" src="http://localhost:5001/detect" alt="ビデオフィード" className={css({width:'90%',height:'300px',rounded:'15px',margin:'0 auto',pt:'5px'})} />
+            <h3 className={css({fontSize:'18px',fontWeight:'bold',textAlign:'center',pt:'5px'})}>余っている食材達</h3>
+            <ul className={css({width:'80%',height:'100px',margin:'0 auto',rounded:'8px',border:'3px solid #FFCE7B',p:'5px',display:'flex',flexWrap:'wrap',gap:'8px'})}>
+                {Array.from(detectedObjects).map((obj, index) => 
+                    <li key={index} className={css({border:'1px solid #FFCE7B',display:'inline-block',rounded:'12px',height:'24px'})}>
+                        <div className={css({display:'flex',alignItems:'center',gap:'3px',flexWrap:'wrap'})}>
+                            <p className={css({fontSize:'14px'})}>{obj}</p>
+                            <div className={css({width:'1px',height:'16px',bg:'#333'})}></div>
+                            <FontAwesomeIcon icon={faXmark}/>
+                        </div>
+                    </li>
+                )}
             </ul>
         </main>
     );

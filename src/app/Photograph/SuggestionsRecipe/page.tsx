9070d7080df7d7982fa9a92fd582df17f967f1b8
koudaihirata <2230051@ecc.ac.jp>
2024-07-06 21:00:18 +0900

    react-tinder-cardを実装&サンプルコード実装

--- src/app/Photograph/SuggestionsRecipe/page.tsx
+++ src/app/Photograph/SuggestionsRecipe/page.tsx
@@ -1,9 +1,24 @@
-
+"use client"
+import TinderCard from 'react-tinder-card';
 
 export default function SuggestionsRecipe() {
-    return(
-        <main>
-            <p>SuggestionsRecipe</p>
-        </main>
-    )
-}
+    const data = ['red', 'blue', 'green'];
+  
+    const swiped = (direction: string, color: string) => {
+      console.log('You swiped ' + direction + ' on ' + color)
+    }
+  
+    const outOfFrame = (color: string) => {
+      console.log(color + ' has left the screen')
+    }
+    return (
+      <main>
+        {data.map((color, index) => (
+          <TinderCard key={index} onSwipe={(dir) => swiped(dir, color)} onCardLeftScreen={() => outOfFrame(color)}>
+            <div style={{backgroundColor: color}}>
+              {color}
+            </div>
+          </TinderCard>
+        ))}
+      </main>  
+    );}

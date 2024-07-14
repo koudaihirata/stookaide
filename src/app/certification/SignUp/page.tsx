2dc8f02f6219b01c55fe19af1fed81ffd93a0ed1
koudaihirata <2230051@ecc.ac.jp>
2024-07-15 01:45:13 +0900

    color.tsに赤色追加

--- src/app/certification/SignUp/page.tsx
+++ src/app/certification/SignUp/page.tsx
@@ -1,3 +1,4 @@
+import { redColor } from "@/style/color";
 import { css } from "../../../../styled-system/css";
 
 
@@ -8,7 +9,12 @@ export default function SignUp() {
             <div className={css({w:'80%',m:'0 auto'})}>
                 <h1 className={css({fontSize:'22px',fontWeight:'bold'})}>新規登録</h1>
             </div>
-            <div></div>
+            <div className={css({w:'100%',h:'1px',bg:'rgba(0, 0, 0, 0.25)',mt:'20px'})}></div>
+            <div className={css({w:'80%',m:'0 auto'})}>
+                <div>
+                    <label htmlFor="user">ユーザー名<span style={{color:redColor}}>必須</span></label>
+                </div>
+            </div>
         </main>
     )
 }
--- src/style/color.ts
+++ src/style/color.ts
@@ -2,3 +2,4 @@ export const mainColor = '#FFCE7B';
 export const white = 'white';
 export const subColor = "#FAA755";
 export const accentColor = "#40D2FF";
+export const redColor = '#F44646';

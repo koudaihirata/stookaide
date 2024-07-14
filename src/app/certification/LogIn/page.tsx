292e03fefa6cf55d143013407d63cda19b99a392
koudaihirata <2230051@ecc.ac.jp>
2024-07-15 01:37:45 +0900

    LogInページのh1&type="password"に変更

--- src/app/certification/LogIn/page.tsx
+++ src/app/certification/LogIn/page.tsx
@@ -10,7 +10,7 @@ import { css } from "../../../../styled-system/css";
 export default function LogIn() {
     return(
         <main className={css({w:'100%',h:'100vh',})} style={{backgroundColor:mainColor}}>
-            <Image src='/nameLogo.png' alt="STOOKAide" width={232} height={55} className={css({m:'0 auto 0'})}/>
+            <h1><Image src='/nameLogo.png' alt="STOOKAide" width={232} height={55} className={css({m:'0 auto 0'})}/></h1>
             <form className={css({w:'80%',h:'288px',rounded:'8px',m:'10vh auto 0'})} style={{backgroundColor:white}}>
                 <div className={css({w:'90%',h:'100%',m:'0 auto',display:'flex',alignItems:'center',justifyContent:'center',flexDirection:'column',gap:'24px'})}>
                     <div className={css({width:'100%'})}>
@@ -19,7 +19,7 @@ export default function LogIn() {
                     </div>
                     <div className={css({width:'100%'})}>
                         <label htmlFor="ps">パスワード</label>
-                        <input type="text" name="ps" id="ps"  className={css({w:'100%',h:'44px',border:'1px solid #3e3e3e',rounded:'8px',pl:'12px'})} placeholder="パスワードを入力してください"/>
+                        <input type="password" name="ps" id="ps"  className={css({w:'100%',h:'44px',border:'1px solid #3e3e3e',rounded:'8px',pl:'12px'})} placeholder="パスワードを入力してください"/>
                     </div>
                     <div className={css({fontSize:'22px',display:'inline-block'})}>
                         <Btn label='ログイン'/>
--- src/app/certification/SignUp/page.tsx
+++ src/app/certification/SignUp/page.tsx
@@ -1,10 +1,14 @@
+import { css } from "../../../../styled-system/css";
 
 
 
 export default function SignUp() {
     return(
         <main>
-            <p>SignUp</p>
+            <div className={css({w:'80%',m:'0 auto'})}>
+                <h1 className={css({fontSize:'22px',fontWeight:'bold'})}>新規登録</h1>
+            </div>
+            <div></div>
         </main>
     )
 }

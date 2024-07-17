1f8e0fe9612a9ac88d1295a0c21e82f86e8517e7
koudaihirata <2230051@ecc.ac.jp>
2024-07-18 00:29:12 +0900

    プロフィール画像の周りのアニメーション完成

--- src/app/Profile/page.tsx
+++ src/app/Profile/page.tsx
@@ -2,6 +2,7 @@
 
 import ProfileMain from "@/components/ProfileMain/ProfileMain";
 import { css } from "../../../styled-system/css";
+import Btn from "@/components/Btn";
 
 
 export default function Profile() {
@@ -9,6 +10,9 @@ export default function Profile() {
         <>
             <main className={css({h:'100vh'})}>
                 <ProfileMain/>
+                <div className={css({w:'100%',display:'flex',justifyContent:'center'})}>
+                    <Btn label="ログアウト"/>
+                </div>
             </main>
         </>
     )
--- src/components/ProfileMain/ProfileMain.tsx
+++ src/components/ProfileMain/ProfileMain.tsx
@@ -4,8 +4,9 @@ import Image from "next/image";
 import { css } from "../../../styled-system/css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGears } from "@fortawesome/free-solid-svg-icons";
-import { mainColor } from "@/style/color";
+import { mainColor, subColor } from "@/style/color";
 import Link from "next/link";
+import "@/components/ProfileMain/ProfileMain.scss";
 
 export default function ProfileMain() {
     return(
@@ -14,8 +15,14 @@ export default function ProfileMain() {
                 <Link href={'#'} >
                     <FontAwesomeIcon icon={faGears} style={{color:mainColor}} className={css({fontSize:'32px',position:'absolute',top:'14px',right:'20px'})}/>
                 </Link>
-                <Image src={'/AdobeStock_724935013.jpg'} alt="プロフィール写真" width={100} height={100} className={css({w:'100px',h:'100px',rounded:'50%'})}/>
-                <h2 className={css({fontSize:'32px',fontWeight:'bold',mt:'12px'})}>晃大</h2>
+                <div className={css({w:'100%',h:'150px',display:'flex',alignItems:'center',justifyContent:'center'})}>
+                    <div className="ProfileMain" style={{backgroundColor:mainColor}}>
+                        <div className="ProfileSub" style={{backgroundColor:subColor}}>
+                            <Image src={'/AdobeStock_724935013.jpg'} alt="プロフィール写真" width={100} height={100} className={css({w:'100px',h:'100px',rounded:'50%'})}/>
+                        </div>
+                    </div>
+                </div>
+                <h2 className={css({fontSize:'32px',fontWeight:'bold',mt:'2px'})}>晃大</h2>
                 <p className={css({fontSize:'16px',fontWeight:'bold'})}>hiratakoudai61@gmail.com</p>
             </div>
         </>

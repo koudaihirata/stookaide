f6e1823a46916f8692323547047edf2a0302e037
koudaihirata <2230051@ecc.ac.jp>
2024-07-18 15:52:20 +0900

    ProfileFavoriteコンポーネント準備

--- src/app/Profile/page.tsx
+++ src/app/Profile/page.tsx
@@ -3,6 +3,7 @@
 import ProfileMain from "@/components/ProfileMain/ProfileMain";
 import { css } from "../../../styled-system/css";
 import Btn from "@/components/Btn";
+import ProfileFavorite from "@/components/ProfileFavorite";
 
 
 export default function Profile() {
@@ -10,6 +11,7 @@ export default function Profile() {
         <>
             <main className={css({h:'100vh'})}>
                 <ProfileMain/>
+                <ProfileFavorite/>
                 <div className={css({w:'100%',display:'flex',justifyContent:'center'})}>
                     <Btn label="ログアウト"/>
                 </div>

b5028f1b33624c3c21ffd35c8765c51b35cc178c
koudaihirata <2230051@ecc.ac.jp>
2024-07-19 10:17:15 +0900

    dbのプロフィールを表示

--- src/components/ProfileMain/ProfileMain.tsx
+++ src/components/ProfileMain/ProfileMain.tsx
@@ -7,8 +7,53 @@ import { faGears } from "@fortawesome/free-solid-svg-icons";
 import { mainColor, subColor } from "@/style/color";
 import Link from "next/link";
 import "@/components/ProfileMain/ProfileMain.scss";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+interface User {
+    created_at: string;
+    date_of_birth: string;
+    email: string;
+    favorite_recipe: string | null;
+    gender: string;
+    id: number;
+    postal_code: string;
+    profile_image: string | null;
+    username: string;
+}
+
+interface AccessToken {
+    message: string;
+    user: User;
+}
 
 export default function ProfileMain() {
+    const router = useRouter();
+    const [useToken, setTokens] = useState<AccessToken | null>(null);
+
+    useEffect(() => {
+        const checkTokens = async () => {
+            const accessToken = JSON.parse(localStorage.getItem('accessToken') || 'null');
+            const refreshToken = JSON.parse(localStorage.getItem('refreshToken') || 'null');
+
+            console.log(accessToken);
+            // console.log(accessToken.user.username); 名前の指定
+
+            
+            if (!accessToken && !refreshToken) {
+                // トークンがない場合、ログインページにリダイレクト
+                router.push('/certification/LogIn');
+            } else {
+                setTokens(accessToken);
+            }
+        };
+
+        checkTokens();
+    }, [router]);
+
+    const username = useToken ? useToken.user.username : "ストッくん";
+    const email = useToken ? useToken.user.email : "*****@stook.com";
+
     return(
         <>
             <div className={css({w:'100%',h:'280px',borderBottom:'1px solid rgba(130, 130, 130, 0.25)',position:'relative',display:'flex',alignItems:'center',justifyContent:'center',flexDirection:'column'})}>
@@ -22,8 +67,8 @@ export default function ProfileMain() {
                         </div>
                     </div>
                 </div>
-                <h2 className={css({fontSize:'32px',fontWeight:'bold',mt:'2px'})}>晃大</h2>
-                <p className={css({fontSize:'16px',fontWeight:'bold'})}>hiratakoudai61@gmail.com</p>
+                <h2 className={css({fontSize:'32px',fontWeight:'bold',mt:'2px'})}>{username}</h2>
+                <p className={css({fontSize:'16px',fontWeight:'bold'})}>{email}</p>
             </div>
         </>
     )  

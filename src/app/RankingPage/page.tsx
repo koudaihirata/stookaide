b0cb11f602542f9237a8cdd3b31fc02f2b926af0
koudaihirata <2230051@ecc.ac.jp>
2024-08-08 22:09:14 +0900

    おすすめの食材コンポーネント作成

--- src/app/RankingPage/page.tsx
+++ src/app/RankingPage/page.tsx
@@ -1,3 +1,4 @@
+import RecommendationR from "@/components/RecommendationR";
 import { css } from "../../../styled-system/css";
 import LeftoversR from "@/components/LeftoversR";
 
@@ -5,10 +6,10 @@ export default function RankingPage() {
     return(
         <main>
             <h2 className={css({textAlign:'center',fontSize:'24px',fontWeight:'bold',pt:'34px'})}>あなたのランキング</h2>
-            <p className={css({fontSize:'14px',textAlign:'center',mt:'20px'})}>あなたが撮ってくれた物の色々なランキングを見てみよう！</p>
-            <div>
-                <LeftoversR/>
-            </div>
+            <p className={css({fontSize:'14px',textAlign:'center',mt:'20px'})}>あなたが撮ってくれた物の<br/>色々なランキングを見てみよう！</p>
+            <LeftoversR/>
+            <RecommendationR/>
+            <p className={css({fontSize:'14px',textAlign:'center',mt:'40px',color:'#8F8F8F'})}>今後のアップデートでランキングが増える予定です<br/>お楽しみに！！</p>
         </main>
     )
 }
---
+++ src/components/RecommendationR.tsx
@@ -0,0 +1,27 @@
+import { mainColor, white } from "@/style/color";
+import { css } from "../../styled-system/css";
+
+
+export default function RecommendationR() {
+    return(
+        <div className={css({w:'100%',h:'30vh',borderTop:'10px solid #FFCE7B',position:'relative',mt:'70px',bg:'rgba(255,206,123,.3)',display:'flex',alignItems:'center',justifyContent:'center',flexDirection:'column',gap:'20px'})}>
+        <div className={css({w:'100%',display:'flex',alignItems:'center',justifyContent:'center',position:'absolute',top:'-40px'})}>
+            <div className={css({w:'65px',h:'65px',rounded:'50%',transform:'translateX(20px)',bg:'#FAF355'})}></div>
+            <h3 className={css({w:'230px',h:'40px',textAlign:'center',lineHeight:'40px',fontWeight:'bold'})} style={{backgroundColor:mainColor,color:white}}>余り物のランキング</h3>
+            <div className={css({w:'65px',h:'65px',rounded:'50%',transform:'translateX(-20px)',bg:'#58FA55'})}></div>
+        </div>
+        <h4 className={css({fontSize:'18px',fontWeight:'bold'})}>
+            １.<span className={css({fontSize:'26px',fontWeight:'bold'})}>牛肉</span>
+        </h4>
+        <div className={css({w:'100%',display:'flex',alignItems:'center',justifyContent:'space-around'})}>
+            <p className={css({fontSize:'18px',fontWeight:'bold'})}>２.<span className={css({fontSize:'20px',fontWeight:'bold'})}>ハム</span></p>
+            <p className={css({fontSize:'18px',fontWeight:'bold'})}>４.<span className={css({fontSize:'20px',fontWeight:'bold'})}>ジャガイモ</span></p>
+        </div>
+        <div className={css({w:'100%',display:'flex',alignItems:'center',justifyContent:'space-around'})}>
+            <p className={css({fontSize:'18px',fontWeight:'bold'})}>３.<span className={css({fontSize:'20px',fontWeight:'bold'})}>ひき肉</span></p>
+            <p className={css({fontSize:'18px',fontWeight:'bold'})}>５.<span className={css({fontSize:'20px',fontWeight:'bold'})}>ほうれん草</span></p>
+        </div>
+    </div>
+
+    )
+}
